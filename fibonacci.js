a4f9d86984fd6154fc44376b7929c5a53ad8c1ca
Juans <sebastian.ochoa.dev@gmail.com>
2024-06-15 17:17:15 -0500

    feat: Add memoized in function fibMemo

--- fibonacci.js
+++ fibonacci.js
@@ -11,3 +11,17 @@ function fib(n) {
 // console.log(fib(6)); // 8
 // console.log(fib(8)); // 21
 console.log(fib(50)); // 12586269025
+
+/**
+ * Memoization
+ * js object, keys will be arg to fn, value will the be return value
+ */
+
+function fibMemo(n, memo = {}) {
+  if (n in memo) return memo[n];
+  if (n <= 2) return 1;
+  memo[n] = fibMemo(n - 1, memo) + fibMemo(n - 2, memo);
+  return memo[n];
+}
+
+console.log(fibMemo(50)); // 12586269025
