59ad2e8e26509c06d3675ec2efcb412df005a0dd
Juans <sebastian.ochoa.dev@gmail.com>
2024-11-13 23:04:13 -0500

    feat: recursive

---
+++ recursive.js
@@ -0,0 +1,31 @@
+//Factorial
+const factorial = (number) => {
+  if (number === 0 || number === 1) return 1;
+  return number * factorial(number - 1);
+};
+
+const isPalindrome = (string = "") => {
+  if (string.length <= 1) return true;
+  if (string.slice(0, 1) !== string.slice(-1)) {
+    return false;
+  }
+
+  return isPalindrome(string.slice(1, -1));
+};
+
+const power = (x = 0, n = 0) => {
+  if (n === 0) return 1;
+
+  if (n < 0) {
+    return 1 / power(x, -n);
+  }
+
+  if (n > 0 && n % 2 === 0) {
+    const y = power(x, n / 2);
+    return y * y;
+  } else {
+    return power(x, n - 1) * x;
+  }
+};
+
+console.log(power(2, -2));
